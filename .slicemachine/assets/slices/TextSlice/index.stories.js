ade8152b3872403470347342ba00d55856c67f39
lihbr <lihbr@users.noreply.github.com>
2022-02-05 12:26:12 +0100

    chore: version slicemachine folder

---
+++ .slicemachine/assets/slices/TextSlice/index.stories.js
@@ -0,0 +1,26 @@
+import MyComponent from '../../../../slices/TextSlice';
+import SliceZone from 'vue-slicezone'
+
+export default {
+  title: 'slices/TextSlice'
+}
+
+
+export const _DefaultSlice = () => ({
+  components: {
+    MyComponent,
+    SliceZone
+  },
+  methods: {
+    resolve() {
+      return MyComponent
+    }
+  },
+  data() {
+    return {
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"text_slice","items":[],"primary":{"description":[{"type":"paragraph","text":"Dolor minim tempor qui ut deserunt sit amet. Magna fugiat Lorem aute ut.","spans":[]}]},"id":"_DefaultSlice"}
+    }
+  },
+  template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
+})
+_DefaultSlice.storyName = 'Default slice'
